8c66db8ba517f58717364723dea9c4f5db9bee3c
MarceloCarabajal <marce.tsgia@gmail.com>
2025-10-31 11:45:14 -0300

    feat(jugador): agregar vidas y puntaje con getters; init en ctor

--- juego/Jugador.h
+++ juego/Jugador.h
@@ -11,6 +11,12 @@ public:
 	void checkCollision(const sf::FloatRect& platformBounds);
 	const sf::FloatRect getBounds() const { return _body.getGlobalBounds(); }
 	
+	// atributos de juego
+	int getVidas() const { return _vidas; }
+	int getPuntaje() const { return _puntaje; }
+	void sumarPuntos(int p) { _puntaje += p; }
+	void perderVida() { if (_vidas > 0) _vidas--; }
+	
 	bool isOnGround() const { return _enSuelo; }
 
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
@@ -21,4 +27,6 @@ private:
 	float _gravedad;
 	float _velocidadSalto;
 	bool _enSuelo;
+	int _vidas;
+	int _puntaje;
 };
